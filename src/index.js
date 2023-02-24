960944288e193e5736057902edaf07a02e88693f
imranhossainemi <blur-imran@hotmail.com>
2023-02-24 20:16:13 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import './style.css';
 import List from './list.js';
+// import interactive from './interactive.js';
 
 const list = new List();
 
 list.addTask();
 list.showTask();
+// interactive();
+const emi = document.getElementById('emi');
+const label = document.querySelector('.label');
+const todo = document.querySelector('.todo');
+emi.addEventListener('click', (e) => {
+  e.preventDefault();
+  todo.classList.toggle('completed');
+});
+label.addEventListener('click', (e) => {
+  e.preventDefault();
+  todo.classList.toggle('editting');
+});
