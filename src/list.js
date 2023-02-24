604a4ffd0b5558daaa3b4c89ce20b4b9701ebe5c
imranhossainemi <blur-imran@hotmail.com>
2023-02-24 21:52:24 +0600

    Update list.js

--- src/list.js
+++ src/list.js
@@ -10,7 +10,7 @@ export default class List {
     document.querySelector('form').addEventListener('submit', () => {
       const description = document.getElementById('new-item').value;
       const completed = false;
-      const index = 0;
+      let index = 1;
       if (description) {
         const newTask = {
           description,
@@ -20,6 +20,7 @@ export default class List {
         listArr.push(newTask);
         localStorage.setItem('task', JSON.stringify(listArr));
         this.showTask();
+        index += 1;
       }
     });
   };
@@ -29,23 +30,24 @@ export default class List {
       document.querySelector('#item-list').innerHTML = 'No task Added';
     } else {
       const doList = document.querySelector('#item-list');
+      let li = '';
       listArr.forEach((elem, index) => {
-        const li = document.createElement('li');
-        li.classList.add('todo');
-        li.innerHTML = `
+        li += `
+        <li class="todo">
         <button class="tog" type="button" title="check!" alt="check!" tabindex="0" id="emi">
           <i class="fa-sharp fa-solid fa-square-check"></i>
         </button>
         
         <div class="view">
-          <label class="label" tabindex="0" for="${elem.index}">${elem.description}</label>
-          <input type="text" class="edit input" maxlength="255" name="${elem.index}" value="${elem.description}" >
+          <label class="label" tabindex="0" for="${elem.index + index}">${elem.description}</label>
+          <input type="text" class="edit input" maxlength="255" id="${index}" name="${elem.index + index}" value="${elem.description}" >
         </div>
     
         <div class="ver"><i class="fa-sharp fa-solid fa-ellipsis-vertical"></i></div>
         <button type="button" class="trash" id="${index}"><i class="fa-sharp fa-solid fa-trash"></i></button>
+        </li>
         `;
-        doList.appendChild(li);
+        doList.innerHTML = li;
       });
     }
     const removeTask = () => {
@@ -57,9 +59,24 @@ export default class List {
         listArr = listArr.filter((element, index) => index !== removeId);
         obj = listArr;
         localStorage.setItem('task', JSON.stringify(obj));
+        window.location.reload();
         this.showTask();
       }));
     };
     removeTask();
+    const editTask = () => {
+      const edit = document.querySelectorAll('.edit');
+      edit.forEach((et) => et.addEventListener('dblclick', () => {
+        const editId = parseInt(et.id, 10);
+        let obj = JSON.parse(localStorage.getItem('task'));
+        listArr = obj;
+        listArr[editId].description = document.getElementById(editId).value;
+        obj = listArr;
+        localStorage.setItem('task', JSON.stringify(obj));
+        window.location.reload();
+        this.showTask();
+      }));
+    };
+    editTask();
   };
 }
